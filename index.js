7030f613b8ede00bb3f085f62f43b94e9712ef9d
Hemant Yadav <hemantyadav1137@gmail.com>
2024-12-12 10:47:25 +0530

    Update Dockerfile, CI/CD workflow, and ignore files; enhance logging and environment variable management

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ dbConnect();
 
 // default route
 app.get("/", (req, res) => { 
-    res.send(`<h1>Welcome here , using CI/CD and Github Actions ${process.env.MONGO_URI} ${process.env.PORT}  hehe </h1>`);
+    res.send(`<h1>Welcome here , using CI/CD and Github Actions at ${process.env.PORT} PORT  hehe </h1>`);
 })
