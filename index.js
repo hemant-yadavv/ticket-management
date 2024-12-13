01d11cdf82f03184c313024fd042f9abeac217cf
Hemant Yadav <hemantyadav1137@gmail.com>
2024-12-13 10:18:58 +0530

    testing

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ dbConnect();
 
 // default route
 app.get("/", (req, res) => { 
-    res.send(`<h1>Welcome here , using CI/CD and Github Actions at ${process.env.PORT} PORT  hehe 2.0</h1>`);
+    res.send(`<h1>Welcome here , using CI/CD and Github Actions at ${process.env.PORT} PORT  hehe 2.0 test</h1>`);
 })
